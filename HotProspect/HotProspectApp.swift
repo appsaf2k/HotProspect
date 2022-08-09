0bccee1db4f5619cc15c0d0ab7fa4c2652936a07
appsaf2k <appsaf2k@gmail.com>
2022-08-09 07:46:12 +0300

    Initial Commit

---
+++ HotProspect/HotProspectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectApp.swift
+//  HotProspect
+//
+//  Created by @andreev2k on 09.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
